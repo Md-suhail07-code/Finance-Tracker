f7dbd4117afe6062b197fccc17ec245f117fafc5
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-05-18 21:58:51 +0530

    installed tailwind css

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+﻿const Login = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900">
+      <div className="rounded-full bg-gray-800 p-6">
+        <h1 className="text-center text-2xl font-bold text-white">
+          Welcome to finTrack
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Login
