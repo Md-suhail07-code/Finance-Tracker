57d7947bd93ae8a4805d5fb8e0ebbc28cb6478e4
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-05-20 22:50:41 +0530

    Added redux functionality

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Toaster } from './components/ui/sonner.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/app/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </StrictMode>,
-)
+);
