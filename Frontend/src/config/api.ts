a67bd6ae94e38f3b5bea80417d88c6e70f65c712
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-06-01 22:15:15 +0530

    Created global api.ts matching professional industry standards

--- Frontend/src/config/api.ts
+++ Frontend/src/config/api.ts
@@ -1,1 +1,18 @@
-export const API_BASE_URL : String = "http://localhost:5000/api";
+import axios from "axios";
+import { useAppSelector } from "@/redux/hooks/reduxHooks";
+
+export const API_BASE_URL: string = "http://localhost:5000/api";
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = useAppSelector((state) => state.auth.token);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
