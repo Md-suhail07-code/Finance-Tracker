27288d5ea74d2ddb2e47a0d4dffa7e3f39e8b14a
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-06-05 21:53:11 +0530

    Monthly comparison route

--- Backend/src/routes/analyticRoutes.js
+++ Backend/src/routes/analyticRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAnalytics } from "../controllers/analyticsController.js";
+import { getAnalytics, monthlyComparision } from "../controllers/analyticsController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/overview", protect, getAnalytics);
+router.get("/monthly-comparison", protect, monthlyComparision);
 
 export default router;
