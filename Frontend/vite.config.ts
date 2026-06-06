9e79be087645eaae67df48863854a89ff7127b65
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-06-06 22:26:26 +0530

    Added dependencies for nivo package

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,17 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ["@nivo/bar", "@nivo/core", "@nivo/axes", "@nivo/scales"],
+    force: true,
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
