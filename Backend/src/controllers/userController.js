f09b527f54b17b3730e91b683896a237e2ab051f
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-06-16 22:34:17 +0530

    Updated getProfile controller

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -3,128 +3,173 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    try{
-        if(!name || !email || !password){
-            return res.status(400).json({
-                success: false,
-                message: "Name, email and password are required"
-            })
-        }
-        const existingUser = await prisma.user.findUnique({
-            where: { email }
-        })
-
-        if(existingUser){
-            return res.status(400).json({
-                success: false,
-                message: "User already exists"
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = await prisma.user.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword
-            }
-        })
-        res.status(201).json({
-            success: true,
-            message: "User registered successfully",
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email
-            }
-        })
+  const { name, email, password } = req.body;
+  try {
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, email and password are required",
+      });
     }
-    catch(error){
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server error",
-            error: error.message
-        })
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
     }
-}
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
+};
 
 export const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        if(!email || !password){
-            return res.status(400).json({
-                success: false,
-                message: "Email and password are required"
-            })
-        }
-
-        const user = await prisma.user.findUnique({
-            where: { email }
-        })
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                message: "User does not exist"
-            })
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if(!isPasswordValid){
-            return res.status(400).json({
-                success: false,
-                message: "Invalid password"
-            })
-        }
-
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "7d" });
-
-        return res.status(200).json({
-            success: true,
-            message: "User logged in successfully",
-            token,
-            user: {
-                id: user.id,
-                name: user.name,
-                email: user.email
-            }
-        })
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server error",
-            error: error.message
-        })
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required",
+      });
     }
-}
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
+};
 
 export const getProfile = async (req, res) => {
-    try{
-        const user = await prisma.user.findUnique({
-            where: { id: req.user.id },
-            select: { id: true, name: true, email: true }
-        })
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user.id },
+      select: { id: true, name: true, email: true },
+    });
 
-        if(!user){
-            return res.status(404).json({
-                success: false,
-                message: "User not found"
-            })
-        }
+    if (!user) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
 
-        return res.status(200).json({
-            success: true,
-            message: "User profile fetched successfully",
-            user
-        })
-    }
-    catch(error){
-        return res.status(500).json({
-            success: false,
-            message: "Internal Server error",
-            error: error.message
-        })
-    }
-}
+    const transactions = await prisma.transaction.findMany({
+      where: { userId: req.user.id },
+      include: { category: true },
+    });
+
+    const categoryMap = {};
+    const monthlyMap = {};
+    const totalTransactions = transactions.length;
+    let totalIncome = 0;
+    let totalExpense = 0;
+
+    const monthArray = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+    transactions.forEach((tran) => {
+      if (tran.type === "INCOME") totalIncome += tran.amount;
+      if (tran.type === "EXPENSE") {
+        totalExpense += tran.amount;
+
+        const catName = tran.category.name;
+        categoryMap[catName] = (categoryMap[catName] || 0) + tran.amount;
+
+        const month = tran.date.getMonth(); // added ()
+        const monthName = monthArray[month];
+        monthlyMap[monthName] = (monthlyMap[monthName] || 0) + tran.amount;
+      }
+    });
+
+    const maxCategory = Object.entries(categoryMap).reduce(
+      (max, [name, amount]) => amount > max.amount? { name, amount } : max,
+      { name: null, amount: 0 }
+    );
+
+    const maxSpendMonth = Object.entries(monthlyMap).reduce(
+      (max, [month, amount]) => amount > max.amount? { month, amount } : max,
+      { month: null, amount: 0 }
+    );
+
+    const totalSaved = totalIncome - totalExpense;
+    const currentMonth = new Date().getMonth() + 1; // +1 to avoid 0 in Jan
+    const avgExpense = currentMonth? totalExpense / currentMonth : 0;
+
+    return res.status(200).json({
+      success: true,
+      message: "User profile fetched successfully",
+      user,
+      totalTransactions,
+      totalIncome,
+      totalExpense,
+      totalSaved,
+      avgExpense,
+      maxCategory,
+      maxSpendMonth
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server error",
+      error: error.message,
+    });
+  }
+};
