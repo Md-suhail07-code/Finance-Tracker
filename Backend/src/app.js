1f9e31dd1b0f75cbbef16baf9de020f786f89e66
Md-suhail07-code <suhailahmedmd40@gmail.com>
2026-09-03 20:14:57 +0530

    Added code to run frontend and backend in the same folder

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/authRoute.js";
 import transactionRoutes from "./routes/transactionRoute.js";
 import categoryRoutes from "./routes/categoryRoute.js";
@@ -16,9 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Finance Tracker API is running");
-});
+app.use(express.static(path.join(path.resolve(), "public")));
+
 
 // Routes
 app.use("/api/auth", authRoutes);
